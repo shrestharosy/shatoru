ce45d84d6c526a39a2bdd5f237f70ff3a5ddc2dc
rosy <rosyshrestha028@gmail.com>
2022-11-06 12:48:52 -0500

    [UMDSS-7] [style] Fix tailwind imports

--- src/components/LogoTemplate.tsx
+++ src/components/LogoTemplate.tsx
@@ -1,23 +1,20 @@
 import React, { FC } from 'react';
 import { View, Image, Text } from 'react-native';
-import { useTailwind } from 'tailwind-rn/dist';
+import tw from 'src/styles/tailwind';
+
 interface LogoProps {
     image: string;
 }
 
 export const LogoTemplate: FC<LogoProps> = ({ image }) => {
-    console.log(image);
-    const tailwind = useTailwind();
     return (
-        <View style={tailwind('py-16 px-8 bg-lightGray rounded-xl w-full')}>
+        <View style={tw`py-16 px-8 bg-lightGray rounded-xl w-full`}>
             <Image
                 source={require('../screens/login/loginpage_logo.png')}
-                style={tailwind('h-32 w-full')}
+                style={tw`h-32 w-full`}
             />
             <Text
-                style={tailwind(
-                    'text-brownRed text-base text-center mt-2 font-bold'
-                )}
+                style={tw`text-brownRed text-base text-center mt-2 font-bold`}
             >
                 UMD Shuttle Service
             </Text>
