21d2b8ec89387916bef415944947bb5c8f80c59a
rosy <rosyshrestha028@gmail.com>
2022-12-04 16:17:08 -0500

    [UMDSS-40] [feat] Allow users to select days of the week

--- src/screens/shuttle/create/details/primary.tsx
+++ src/screens/shuttle/create/details/primary.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Controller, useFormContext } from 'react-hook-form';
 import { View } from 'react-native';
+import CustomMultiSelect from 'src/components/form/CustomMultiSelect';
 import { CustomInput } from 'src/components/form/CustomTextInput';
+import { Label } from 'src/components/form/Label';
 import TimePicker from 'src/components/form/TimePicker';
+import { DAYS_OPTIONS } from 'src/libs/constants/day';
 import tw from 'src/styles/tailwind';
 
 const PrimaryDetails = () => {
+    const [selectedOptions, setSelectedOptions] = React.useState<Array<string>>(
+        []
+    );
     const formContext = useFormContext();
 
-    const { control } = formContext;
+    const { control, setValue } = formContext;
 
     return (
         <>
@@ -24,7 +30,26 @@ const PrimaryDetails = () => {
                         />
                     )}
                 />
-                <View style={tw`flex flex-row justify-between p-2`}>
+                <View>
+                    <Label label={'Select Days'} />
+                    <Controller
+                        name="days"
+                        control={control}
+                        render={({ field }) => (
+                            <CustomMultiSelect
+                                name="days"
+                                showSelectedOptionsAsTags={true}
+                                options={DAYS_OPTIONS}
+                                selectedOptions={selectedOptions}
+                                onChange={(item) => {
+                                    setSelectedOptions(item);
+                                    setValue('days', item);
+                                }}
+                            />
+                        )}
+                    />
+                </View>
+                <View style={tw`flex flex-row justify-between p-2 mt-3`}>
                     <View>
                         <Controller
                             name="startTime"
