9b2cec8cfa7600bb35749046c3d027de7f73c2b8
rosy <rosyshrestha028@gmail.com>
2022-12-04 12:44:21 -0500

    [UMDSS-40] [feat] Add platform specific code for android

--- src/screens/shuttle/create/details/primary.tsx
+++ src/screens/shuttle/create/details/primary.tsx
@@ -1,15 +1,46 @@
-import DateTimePicker from '@react-native-community/datetimepicker';
-import React from 'react';
+import DateTimePicker, {
+    DateTimePickerAndroid,
+} from '@react-native-community/datetimepicker';
+import React, { useState } from 'react';
 import { Controller, useFormContext } from 'react-hook-form';
-import { View } from 'react-native';
+import { Button, Text, View } from 'react-native';
 import { CustomInput } from 'src/components/form/CustomTextInput';
 import { Label } from 'src/components/form/Label';
+import { isIOS } from 'src/libs/util/platform';
 import tw from 'src/styles/tailwind';
 
 const PrimaryDetails = () => {
     const formContext = useFormContext();
 
+    const [date, setDate] = useState<Date>(new Date());
+
+    const openDatePickHandler = () => {
+        DateTimePickerAndroid.open({
+            mode: 'time',
+            value: date,
+            onChange: (event, newDate) => {
+                if (newDate) {
+                    DateTimePickerAndroid.open({
+                        mode: 'time',
+                        value: newDate,
+                        onChange: (_, newDateTime) => {
+                            if (newDateTime) {
+                                setDate(newDateTime);
+                            }
+                        },
+                    });
+                }
+            },
+        });
+    };
+
     const { control, setValue } = formContext;
+
+    const parseTime = (timestamp: Date) => {
+        const hour = new Date(timestamp).getHours();
+        const minute = new Date(timestamp).getMinutes();
+        return `${hour} : ${minute}`;
+    };
 
     return (
         <>
@@ -33,22 +64,28 @@ const PrimaryDetails = () => {
                             render={({ field }) => (
                                 <>
                                     <Label label="Start Time" />
-
-                                    <DateTimePicker
-                                        value={field.value ?? new Date()}
-                                        mode={'time'}
-                                        onChange={(e) => {
-                                            setValue(
-                                                'startTime',
-                                                new Date(
-                                                    e.nativeEvent.timestamp
-                                                )
-                                            );
-                                        }}
-                                        style={{
-                                            width: 86,
-                                        }}
+                                    <Text>{parseTime(date)}</Text>
+                                    <Button
+                                        title={'Change'}
+                                        onPress={() => openDatePickHandler()}
                                     />
+                                    {isIOS() && (
+                                        <DateTimePicker
+                                            value={field.value ?? new Date()}
+                                            mode={'time'}
+                                            onChange={(e) => {
+                                                setValue(
+                                                    'startTime',
+                                                    new Date(
+                                                        e.nativeEvent.timestamp
+                                                    )
+                                                );
+                                            }}
+                                            style={{
+                                                width: 86,
+                                            }}
+                                        />
+                                    )}
                                 </>
                             )}
                         />
@@ -60,29 +97,28 @@ const PrimaryDetails = () => {
                             render={({ field }) => (
                                 <>
                                     <Label label="End Time" />
-                                    <DateTimePicker
-                                        value={field.value ?? new Date()}
-                                        mode={'time'}
-                                        onChange={(e) => {
-                                            // console.log(
-                                            //     new Date(
-                                            //         e.nativeEvent.timestamp
-                                            //     ).getHours(),
-                                            //     new Date(
-                                            //         e.nativeEvent.timestamp
-                                            //     ).getMinutes()
-                                            // );
-                                            setValue(
-                                                'endTime',
-                                                new Date(
-                                                    e.nativeEvent.timestamp
-                                                )
-                                            );
-                                        }}
-                                        style={{
-                                            width: 100,
-                                        }}
+                                    <Text>{parseTime(date)}</Text>
+                                    <Button
+                                        title="Change"
+                                        onPress={() => openDatePickHandler()}
                                     />
+                                    {isIOS() && (
+                                        <DateTimePicker
+                                            value={field.value ?? new Date()}
+                                            mode={'time'}
+                                            onChange={(e) => {
+                                                setValue(
+                                                    'endTime',
+                                                    new Date(
+                                                        e.nativeEvent.timestamp
+                                                    )
+                                                );
+                                            }}
+                                            style={{
+                                                width: 100,
+                                            }}
+                                        />
+                                    )}
                                 </>
                             )}
                         />
